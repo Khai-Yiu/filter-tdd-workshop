07e047e74d8b54832176326bab56fc93e45231e0
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-02-16 11:09:35 +0800

    RED: Given a callback function and an array › Invokes the callback function passing it an item from the array as a parameter

--- src/filter.test.js
+++ src/filter.test.js
@@ -9,5 +9,12 @@ describe('filter', () => {
             filter(callbackFunction, arrayToFilter)
             expect(callbackFunction).toHaveBeenCalledTimes(numberOfItemsInArrayToFilter)
         })
+        it('Invokes the callback function passing it an item from the array as a parameter', () => {
+            const arrayToFilter = [1, 2]
+            const callbackFunction = jest.fn()
+            filter(callbackFunction, arrayToFilter)
+            expect(callbackFunction.mock.calls[0][0]).toBe(arrayToFilter[0])
+            expect(callbackFunction.mock.calls[1][0]).toBe(arrayToFilter[1])
+        })
     })
 })  
