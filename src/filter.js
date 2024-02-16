378a22ed375bff1085d58d37eaf5c06bc4e3982a
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-02-16 11:13:06 +0800

    GREEN: Given a callback function and an array › returns an array

--- src/filter.js
+++ src/filter.js
@@ -2,6 +2,8 @@ function filter(callbackFunction, arrayToFilter) {
     for(let item of arrayToFilter) {
         callbackFunction(item)
     }
+
+    return arrayToFilter
 }
 
 export default filter
